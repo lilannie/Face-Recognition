1d0130d173dc0602e236b50fb0e3d7f277fb91d9
JackRometty <jwrometty@gmail.com>
2016-09-13 10:55:26 -0500

    flask server update (not decoding image 100%)

--- flask-server-test/server.py
+++ flask-server-test/server.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import request
+import base64
 app = Flask(__name__)
 
 @app.route('/imgsave', methods = ['POST'])
 def hii():
-	print request.form['base64URL']
+	imgdata = request.form['base64URL']
+	print imgdata
+	fh = open("imageToSave.png", "wb")
+	print len(imgdata) % 4
+	fh.write(base64.b64decode(imgdata + '=' * (4 - len(imgdata) % 4)))
+	fh.close()
+
 	return request.data
 
 @app.route('/')
 def hello_world():
     return app.send_static_file('flask_client.html')
+
+
+
